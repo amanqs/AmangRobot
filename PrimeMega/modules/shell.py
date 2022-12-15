549c328fece244b3c6d0c2e1efe8469a3a87edd5
amanqs <116822442+amanqs@users.noreply.github.com>
2022-12-15 18:08:02 +0700

    Update shell.py

--- PrimeMega/modules/shell.py
+++ PrimeMega/modules/shell.py
@@ -42,8 +42,8 @@ def shell(update: Update, context: CallbackContext):
         message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
 
 
-SHELL_HANDLER = CommandHandler(["sh"], shell, run_async=True)
+SHELL_HANDLER = CommandHandler(["bash"], shell, run_async=True)
 dispatcher.add_handler(SHELL_HANDLER)
 __mod_name__ = "Shell"
-__command_list__ = ["sh"]
+__command_list__ = ["bash"]
 __handlers__ = [SHELL_HANDLER]
